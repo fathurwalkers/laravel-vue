6ecb8663fceb3c6eadec35ef1313910329e605d1
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-13 11:53:27 -0800

    updating

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,14 @@
+import Vue from 'vue';
+
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue').default;
 
+Vue.component('test-component', require('./components/TestComponent.vue').default);
 
 const app = new Vue({
     el: '#app',
+    data: {
+        message: 'Hello Semua!',
+    }
 });
---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12">
+                <h2>Belajar Laravel & Vue Js</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        console.log('export berhasil!');
+    }
+}
+</script>
