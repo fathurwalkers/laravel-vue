d9ed2bdfe4a96ae054f60a04d3f1eaeeb8755c95
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-13 16:01:52 -0800

    updating

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackController;
 
 Route::get('/', function () {
     return view('app');
 });
 
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '.*');
+Route::group(['prefix' => '/dashboard'], function () {
+    Route::get('{any}', function () {
+        return view('app');
+    })->where('any', '.*');
+});
+
+Route::get('/generate-data', [BackController::class, 'generate_data'])->name('generate-data');
