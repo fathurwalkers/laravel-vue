4d4781ac88fe799274b24016f25ba95553c8c340
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-13 16:08:29 -0800

    updating

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -3,6 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produk;
+use App\Models\Login;
+use App\Models\Detail;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class BackController extends Controller
 {
