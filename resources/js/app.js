69c6c8628355570cdd847e83882115573eaa9bf7
Fathur Walkers <fathurwalkers44@gmail.com>
2021-03-15 04:51:02 -0700

    updating

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import Vue from 'vue';
 import router from './routes.js';
 
 Vue.component('test-component', require('./components/TestComponent.vue').default);
-
 Vue.component('header-component', require('./components/layouts/Header.vue').default);
 Vue.component('main-component', require('./components/layouts/MainContent.vue').default);
 Vue.component('footer-component', require('./components/layouts/Footer.vue').default);
